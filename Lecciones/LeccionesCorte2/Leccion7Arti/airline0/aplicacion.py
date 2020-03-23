ae9cdb4ac534ade9449018fb33b447cef99244b1
adrianartino <60418445+adrianartino@users.noreply.github.com>
2020-03-23 14:10:52 -0600

    Se ha agregado el Login del Proyecto 1

--- Lecciones/LeccionesCorte2/Leccion7Arti/airline0/aplicacion.py
+++ Lecciones/LeccionesCorte2/Leccion7Arti/airline0/aplicacion.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 engine = create_engine("postgresql://postgres:marcos619@localhost:5432/airline")
 db = scoped_session(sessionmaker(bind=engine))
 
-@app.route("/")
+@app.route("/") #al iniciar la app, se llena un arreglo flights
 def index():
     flights = db.execute("SELECT * FROM vuelos").fetchall()
     return render_template("index.html", flights=flights)
@@ -17,9 +17,9 @@ def book():
     """Book a flight."""
 
     # Get form information.
-    name = request.form.get("name")
+    name = request.form.get("name") #guarda el nombre
     try:
-        flight_id = int(request.form.get("flight_id"))
+        flight_id = int(request.form.get("flight_id")) #guarda el vuelo
     except ValueError:
         return render_template("error.html", message="Invalid flight number.")
 
@@ -27,9 +27,11 @@ def book():
     # Make sure flight exists.
     if db.execute("SELECT * FROM vuelos WHERE id = :id", {"id": flight_id}).rowcount == 0:
         return render_template("error.html", message="No such flight with that id.")
-    db.execute("INSERT INTO pasajeros (nombre, id_vuelo) VALUES (:nombre, :id_vuelo)", {"nombre": name, "id_vuelo": flight_id})
-    db.commit()
-    return render_template("success.html")
+
+    db.execute("INSERT INTO pasajeros (nombre, id_vuelo) VALUES (:nombre, :id_vuelo)", {"nombre": name, "id_vuelo": flight_id}) #insertar nuevo pasajero
+    db.commit() #guardar datos de base de datos
+    return render_template("success.html") #
+
 @app.route("/flights")
 def flights():
     """Lists all flights."""
